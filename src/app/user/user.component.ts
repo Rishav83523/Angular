868055d22ff78dd772faf13df0cd79bd0ea9b23a
Rishav83523 <rishav.raj@calsoftinc.com>
2024-11-20 15:31:09 +0530

    defining components inputs

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -14,19 +11,16 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 //do below so we can access it in html all thing you describe in component will be accessed in html
 export class UserComponent {
   //this is a public property
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
 
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
-  // get imagePath() {
-  //   return 'assets/users/' + this.selectedUser.avatar;
-  // }
+  //@input will be mark this property setable from outside
+  @Input() avatar!: string;
+  @Input() name!: string;
 
-  onSelectUser() {
-    // alert('User selected!');
-    this.selectedUser.set(
-      DUMMY_USERS[Math.floor(Math.random() * DUMMY_USERS.length)]
-    );
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
   }
+
+  onSelectUser() {}
 }
 
 //we are accessing inside class not in template so thats why this.selectedUser.avatar

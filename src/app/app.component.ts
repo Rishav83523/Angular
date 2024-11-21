27bc8379339f8311fcef165f9506d96dbf76ebfa
Rishav83523 <rishav.raj@calsoftinc.com>
2024-11-21 14:19:34 +0530

    my soln

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, output } from '@angular/core';
+import { DUMMY_USERS } from './dummy-users';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
-import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS;
+  selectedUser: string = '';
 
   onSelectUser(id: string) {
     console.log(id);
+    const user = this.users.find((user) => user.id === id);
+    if (user) {
+      console.log(`ID: ${id}, Name: ${user.name}`);
+      this.selectedUser = user.name;
+    } else {
+      console.log(`User with ID: ${id} not found`);
+    }
   }
 }

864982778d884c7e73579ca55fc8020d5895cf27
Rishav83523 <rishav.raj@calsoftinc.com>
2024-11-21 10:56:22 +0530

    working with output and emitting data

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, Input, input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 //Input with upper case is a  decorator with lower case its a special function
 
 @Component({
@@ -14,31 +14,23 @@ export class UserComponent {
   //this is a public property
 
   //@input will be mark this property setable from outside
-  // @Input({ required: true }) avatar!: string;
-  // @Input({ required: true }) name!: string;
+  @Input({ required: true }) id!: string; //mene yeh input apne parent element se liya by using property binding than isi input ko mene output kiya @Output se
+  //phir is output ko mene waha catch kiya is tarah (select)="onSelectUser($event)"
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+  @Output() select = new EventEmitter();
 
-  //below we are saying typescript string will be recieved in future
-  //in this below input we can pass an intial value but when passing required we cannot apply intial value
-
-  avatar = input.required<string>();
-  name = input.required<string>();
-
-  //these input are read only they change when they read some change outside for exaple in app.component.html
-  //we cannot change them inside the class
-
-  //but outside we still set the input as same with property bindings
-  //we no longer need an ! mark because we are not setting up a property which doesnot have an intial value
+  //this select will recieve an intial input unlike @Input and that inital value is EventEmitter
 
   //by setting required:true you are telling angular that property must be there
 
-  imagePath = computed(() => 'assets/users/' + this.avatar());
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
 
-  //with the help of computed we made it signal
-  // get imagePath() {
-  //   return 'assets/users/' + this.avatar;
-  // }
-
-  onSelectUser() {}
+  onSelectUser() {
+    this.select.emit(this.id);
+  }
 }
 
 //we are accessing inside class not in template so thats why this.selectedUser.avatar

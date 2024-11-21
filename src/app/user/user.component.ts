0b978a85e3ea61e8e5ce38404918ba5b1e2a09c8
Rishav83523 <rishav.raj@calsoftinc.com>
2024-11-21 10:35:07 +0530

    using signals as input

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, computed, Input, input } from '@angular/core';
+//Input with upper case is a  decorator with lower case its a special function
 
 @Component({
   selector: 'app-user',
@@ -13,14 +14,29 @@ export class UserComponent {
   //this is a public property
 
   //@input will be mark this property setable from outside
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
+  // @Input({ required: true }) avatar!: string;
+  // @Input({ required: true }) name!: string;
+
+  //below we are saying typescript string will be recieved in future
+  //in this below input we can pass an intial value but when passing required we cannot apply intial value
+
+  avatar = input.required<string>();
+  name = input.required<string>();
+
+  //these input are read only they change when they read some change outside for exaple in app.component.html
+  //we cannot change them inside the class
+
+  //but outside we still set the input as same with property bindings
+  //we no longer need an ! mark because we are not setting up a property which doesnot have an intial value
 
   //by setting required:true you are telling angular that property must be there
 
-  get imagePath() {
-    return 'assets/users/' + this.avatar;
-  }
+  imagePath = computed(() => 'assets/users/' + this.avatar());
+
+  //with the help of computed we made it signal
+  // get imagePath() {
+  //   return 'assets/users/' + this.avatar;
+  // }
 
   onSelectUser() {}
 }
